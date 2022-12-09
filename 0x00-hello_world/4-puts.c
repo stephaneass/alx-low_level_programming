526149dbdd58e568ee2b132be600ba0374e6e4c7
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-08 21:39:33 -0800

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
+ * puts - it is use to print text
  * main: use puts fonction
- * description: usage of puts
+ * Description: usage of puts
  * Return: return 0 if everything is okay
  */
 int main(void)
