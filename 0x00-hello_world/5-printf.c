6478c7b8a9cf46213dc8374f58f9c1e75b4cbe79
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-08 21:49:30 -0800

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * printf - use to print text
+ * Description: use printf to display data
+ * Return: always 0 means success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,%s");
+	return (0);
+}
