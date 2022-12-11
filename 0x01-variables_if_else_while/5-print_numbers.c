c0e2a7d4e4340c4b02d90c213bc723dbdb9a341c
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-11 10:28:59 -0800

    Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * Description: prints Numbers,
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
