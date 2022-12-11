43f26a512accf447c92c42859f3d4ad4488313ee
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-11 10:41:00 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * Description: Smile in the mirror
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	int ch;
+
+	for (ch = 122; ch >= 97; ch++)
+	{
+		putchar("%c", ch);
+	}
+	putchar('\n');
+	return (0);
+}
