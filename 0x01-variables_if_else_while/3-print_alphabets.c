b509c53bd4728399bc8eadbf99c424c6c43b7444
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-11 10:14:07 -0800

    alphABET

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -2,7 +2,7 @@
 
 /**
  * main - Enter point
- * Descriiption: prints the alphABET
+ * Description: prints the alphABET
  * Return: 0 always (success)
  */
 int main(void)
@@ -10,9 +10,13 @@ int main(void)
 	char ch;
 
 	for (ch = 'a'; ch <= 'z'; ch++)
+	{
 		putchar(ch);
+	}
 	for (ch = 'A'; ch <= 'Z'; ch++)                                                                      
+	{
 		putchar(ch);
+	}
 	putchar('\n');
 	return (0);
 }
