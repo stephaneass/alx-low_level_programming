e7f4835c83b17ea1cfa6466b48b42a0d4e13addb
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-11 11:08:24 -0800

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Enter point
+ * Description: Hexadecimal
+ * Return: 0 always (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = '0'; ch <= '9'; ch++)
+		putchar(ch);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch)
+	putchar('\n');
+	return (0);
+}
