191143d26e28f34eca537826c0421c34b1080418
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-12 16:21:33 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,6 +20,8 @@ int main(void)
 			{
 				for (l = i; l <= 9; l++)
 				{
+					if (i == 0 && j == 0 && k == 0 && l == 0)
+						continue;
 					putchar(i + '0');
 					putchar(j + '0');
 					putchar(' ');
