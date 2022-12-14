4820a8355ac1fd9cfa0ecc8dd67062c0fe8ee772
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 16:17:29 -0800

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entre point
+ * Return: 0 always success
+ */
+int main(void)
+{
+	char l[8] = "_putchar";
+	int i;
+	for (i = 0; i < 8; i++)
+	{
+		putchar(l[i]);
+	}
+	return (0);
+}
