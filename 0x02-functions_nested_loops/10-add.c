00c51f3d2bb13159736bacef1ec1232046ee2b57
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 18:27:39 -0800

    a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: number 1 to compute
+ * @b: number 2 to compute
+ * Return: absolute value
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
