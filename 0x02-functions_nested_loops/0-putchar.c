c6039ee17492e184e6f54b8c3d748e769a691908
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 16:25:43 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,13 +5,20 @@
  * main - Entre point
  * Return: 0 always success
  */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
 int main(void)
 {
 	char l[8] = "_putchar";
 	int i;
 	for (i = 0; i < 8; i++)
 	{
-		putchar(l[i]);
+		_putchar(l[i]);
 	}
+	putchar('\n');
 	return (0);
 }
