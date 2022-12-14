caeff6110b9c711fbc740d7d2bcb6e5bb6da742b
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 18:48:14 -0800

    98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,14 +14,20 @@ void print_to_98(int n)
 	{
 		for (i = n; i <= 98; i++)
 		{
-			printf("%d, ", i);
+			if (i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
 		}
 	}
 	else
 	{
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d, ", i);
+			if(i == 98)
+				printf("%d", i);
+			else
+				printf("%d, ", i);
 		}
 	}
 
