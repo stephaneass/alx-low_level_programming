4e7e743f86985b749dac701cf20794274267939e
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 17:25:16 -0800

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,11 +3,11 @@
 /**
  * _islower - checks for lowercase character
  *
- * Return: 0 or 1 
+ * Return: 0 or 1
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return 1;
-	return 0;
+		return (1);
+	return (0);
 }
