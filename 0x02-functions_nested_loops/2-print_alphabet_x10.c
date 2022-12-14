8c033691fa00d226f65ec3891cfd47bc9cbccfc9
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-13 17:08:15 -0800

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+		print_alphabet();
+}
