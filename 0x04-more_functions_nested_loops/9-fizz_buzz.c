6699986e7ada10d7daf6ab2d3b6abb57811306cb
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-16 07:20:33 -0800

    9. Fizz-Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,15 +14,14 @@ int main(void)
 	{
 		if (i % 3 == 0 && i % 5 == 0)
 			printf("FizzBuzz");
-		else 
-			if (i % 3 == 0)
+		else if (i % 3 == 0)
 				printf("Fizz");
-		else 
-			if (i % 5 == 0)
+		else if (i % 5 == 0)
 				printf("Buzz");
 		else
 			printf("%d", i);
 		if (i < 100)
 			printf(" ");
 	}
+	return (0);
 }
