85dcc8de2fde0d3a1cc57d567b51d9ca399c378b
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-20 02:08:47 -0800

    Half the lies they tell about me aren't true

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,8 +10,9 @@
 void puts2(char *str)
 {
 	int count;
+	int lenght = strlen(str);
 
-	for (count = 0; count < strlen(str); count+=2)
+	for (count = 0; count < lenght; count+=2)
 	{
 		if (*(str + count) != '\0')
 		{
