782d62ef97014638af8eab9e134613ecd09c503d
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-19 21:24:52 -0800

    2. This report, by its very length, defends itself against the risk of being read

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ * @s: param that points to
+ * Return: length of a string
+ */
+
+int _strlen(char *s)
+{
+	return (sizeof(*s));
+}
