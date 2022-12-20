1102b91eee5ac1345f98beeda84b3b24780e7896
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-19 21:47:29 -0800

    3. I do not fear computers. I fear the lack of them

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -10,7 +10,7 @@ void _puts(char *str)
 {
 	int count;
 
-	for (count = 0; *(s + count) != '\0'; ++count)
-		_putchar(*(s + count));
+	for (count = 0; *(str + count) != '\0'; ++count)
+		_putchar(*(str + count));
 	_putchar('\n');
 }
