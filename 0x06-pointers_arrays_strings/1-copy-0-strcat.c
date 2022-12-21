784a5c8069a929f0531ec52174150399e2385cd1
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-21 15:28:32 -0800

    0. strcat

--- 0x06-pointers_arrays_strings/1-copy-0-strcat.c
+++ 0x06-pointers_arrays_strings/1-copy-0-strcat.c
@@ -13,9 +13,7 @@ char *_strcat(char *dest, char *src)
 {
 	int i, n;
 
-	i = strlen(dest);
-
-	/*for (i = 0; dest[i] != '\0'; i++);*/
+	for (i = 0; dest[i] != '\0'; i++);
 	for (n = 0; (dest[i + n] = *src++) != '\0'; n++);
 
 	return (dest);
