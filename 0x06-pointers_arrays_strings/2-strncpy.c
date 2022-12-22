8cd21ad9fedc826fe43af29f4c415ea2919f719d
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-21 16:45:55 -0800

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,12 +14,14 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int j;
 
-	for (j = 0; j < n; j++)
+	for (j = 0; src[j] != '\0'; j++)
 	{
-		dest[j] = src[j];
-		if (src[j] == '\0')
-			j = n;
+		if (j < n)
+			dest[j] = src[j];
 	}
+
+	while(j < n)
+		dest[j++] = '\0';
 
 	return (dest);
 }
