040e3397e0f05e6318a8d3e4d5333fdbc6c62f88
Stephane ASSOCLE <mensste30@gmail.com>
2022-12-23 10:40:47 -0800

    Always look up

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -15,7 +15,7 @@ char *string_toupper(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = toupper(str[i]);
+			str[i] = str[i] - 32;
 	}
 
 	return (str);
