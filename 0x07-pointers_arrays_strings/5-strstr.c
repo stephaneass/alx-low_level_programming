f5cd743af89d48bb22e4a347cf5029529597e96f
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-02 11:58:12 -0800

    5. strstr

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strstr - Entry point
+ * @haystack: initial data
+ * @needle: data to search
+ *
+ * Return: a pointer to the byte
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	for (; *haystack != '\0'; haystack++)
+	{
+		char *l = haystack;
+		char *p = needle;
+
+		while (*l == *p && *p != "\0")
+		{
+			l++;
+			p++;
+		}
+		if (*p == "\0")
+			return (haystack);
+	}
+	return ("\0");
+}
