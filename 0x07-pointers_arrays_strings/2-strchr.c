ae60765ac36a85eb70106e6771d52fc32a1308c3
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-02 11:02:42 -0800

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -12,11 +12,11 @@ char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; *s[i] != '\0'; i++)
+	for (; s[i] != '\0'; i++)
 	{
-		if (*s[i] == c)
+		if (s[i] == c)
 			return (&s[i]);
 	}
 
-	return (NULL);
+	return (0);
 }
