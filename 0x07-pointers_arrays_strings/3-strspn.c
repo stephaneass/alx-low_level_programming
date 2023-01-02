c726e997f363281ce26cc731fe4c603012b78892
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-02 12:03:51 -0800

    2. strchr

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -13,9 +13,9 @@ unsigned int _strspn(char *s, char *accept)
 	unsigned int i = 0;
 	int j;
 
-	for (i = 0; s[i] != "\0"; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		for (j = 0; accept[j] != "\0"; j++)
+		for (j = 0; accept[j] != '\0'; j++)
 		{
 			if (s[i] == accept[j])
 			{
