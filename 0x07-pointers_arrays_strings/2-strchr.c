9b232d8a98c4684adb8342cc142a13a343bdb22a
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-02 11:01:37 -0800

    2. strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -5,16 +5,16 @@
  * @s: address that point to the original string
  * @c: the string to look for
  *
- * Return: pointer to the first occurrence of the character 
+ * Return: pointer to the first occurrence of the character
  */
 
 char *_strchr(char *s, char c)
 {
 	int i = 0;
 
-	for (; s[i] != '\0'; i++)
+	for (; *s[i] != '\0'; i++)
 	{
-		if (s[i] == c)
+		if (*s[i] == c)
 			return (&s[i]);
 	}
 
