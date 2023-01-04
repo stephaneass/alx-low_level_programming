d3ff0e05cd02863db258f6fbff95f23e96accb2f
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-04 09:45:08 -0800

    0. She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints a string with a new line
+ * @s: string to print
+ *
+ * Return: anything
+ */
+void _puts_recursion(char *s)
+{
+	if (s != '\0')
+		return;
+	_putchar(s);
+	_puts_recursion(s++);
+}
