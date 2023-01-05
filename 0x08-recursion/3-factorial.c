da008efc653910ba3984aa2d5ac9ae1b09aafc2b
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-04 17:51:06 -0800

    3. You mustn't be afraid to dream a little bigger, darling

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * factorial - the factorial of a given number
+ * @n: the number
+ *
+ * Return: the result
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return -1;
+	if (n < 1)
+		return 1;
+	return (n * factorial(n - 1));
+}
