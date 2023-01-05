c67c09a26e96975a9dc11c66f6a2b865e1654801
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-04 18:27:18 -0800

    _sqrt_recursion

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * search_sqrt - to search the sqrt
+ * @n: the number
+ * @i: the number to compare
+ *
+ * Return: the sqrt
+ */
+int search_sqrt(int n, int i)
+{
+	if (i * i == n)
+		return i;
+	else if (i * i > n)
+		return (-1);
+	else
+		return search_sqrt(n, i);
+}
+/**
+ * _sqrt_recursion - the natural square root of a number
+ * @n: the number
+ *
+ * Return: the result
+ */
+int _sqrt_recursion(int n)
+{
+	search_sqrt(n, 1);
+}
