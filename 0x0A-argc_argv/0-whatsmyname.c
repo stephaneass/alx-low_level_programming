5cc7beb27adfea9ee289495d1689557804d92fe5
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-06 15:56:00 -0800

    0. It ain't what they call you, it's what you answer to

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * @argc: nomber of params
+ * @argv: params vertor
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv)
+{
+	printf("%s", argv[0]);
+}
