753c4a0c6151e75563bac4f10c405706330bbf15
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-06 16:13:01 -0800

    program that multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entery point
+ * @argc: nomber of params
+ * @argv: params vertor
+ *
+ * Return: 0 (success)
+ */
+int main(int argc, char *argv[])
+{
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	printf("%d\n", (argv[1] * argv[2]));
+	return (0);
+}
