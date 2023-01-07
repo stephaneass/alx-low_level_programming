b5c8088ec1d102d89ac71172f004aeb75562124a
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-06 16:49:49 -0800

    program that adds positive numbers

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entery point
+ * @argc: nomber of params
+ * @argv: params vertor
+ *
+ * Return: 0 or 1
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+	int sum = 0;
+
+	if (agrc > 1)
+	{
+		for (i = 0; i < agrc; i++)
+		{
+			if (atoi(argv[i]) > 0)
+			{
+				sum += atoi(argc[i]);
+			}
+			else
+			{
+				printf("Error\n");
+
+				return (1);
+			}
+		}
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
