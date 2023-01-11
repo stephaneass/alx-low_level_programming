c174f196789a0282d0fe822cf6b573686ea28f6b
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-11 09:55:06 -0800

    pointer to a 2 dimensional array of integers

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -26,7 +26,7 @@ int **alloc_grid(int width, int height)
 
 	for (i = 0; i < height; i++)
 	{
-		ar[i] = malloc(sizeof(int) * width);
+		ar[i] = (int *) malloc(sizeof(int) * width);
 		if (ar[i] == NULL)
 			return (NULL);
 		for (j = 0; j < width; j++)
