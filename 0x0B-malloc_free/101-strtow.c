8e6f2e4235d96a55290a012fd6bbe00fb889846b
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-11 18:42:42 -0800

    splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,57 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * strtow - Main Entry
+ * @str: the string
+ *
+ * Return: splits of the string into words
+ */
+
+char **strtow(char *str)
+{
+	char **result;
+	unsigned int i, j, h, c, height;
+
+	if (str == NULL || *str == '\0')
+		return (NULL);
+
+	height = 0;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != ' ' && str[i] == '\0')
+			height++;
+	}
+
+	result = malloc((height + 1) * sizeof(char *));
+
+	if (result == NULL || height == 0)
+	{
+		free(result);
+		return (NULL);
+	}
+
+	for (i = h = 0; i < height; i++)
+	{
+		for (c = h; str[c] != '\0'; c++)
+		{
+			if (str[c] == ' ')
+				a1++;
+			if (str[c] != ' ' && (str[c + 1] == ' ' || str[c + 1] == '\0'))
+			{
+				result[i] = malloc((c - h + 2) * sizeof(char));
+				if (result[i] == NULL)
+				{
+					free_grid(result, i);
+					return (NULL);
+				}
+				break;
+			}
+		}
+		for (j = 0; a1 <= c; a1++, j++)
+			result[i][j] = str[h];
+		result[i][j] = '\0';
+	}
+	result[i] = NULL;
+	return (result);
+}
