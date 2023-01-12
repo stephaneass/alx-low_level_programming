49e68a6475efcb9733331bc3f778d9d96a1616d4
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-12 12:55:21 -0800

    a function that allocates memory using malloc

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * string_nconcat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ *
+ * Return: concatenation result
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *res, i, j, len1, len2;
+
+	len1 = strlen(s1);
+	len2 = 0;
+
+	if (strlen(s2) > 0)
+		
+}
