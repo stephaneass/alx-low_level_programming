740327e070a1e7fd23a637131f9884055ba474f3
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-16 07:44:28 -0800

    new type struct dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,11 +2,6 @@
 #define DOG_H
 
 /**
- * File: dog.h
- * Desc: Header file that defines a new type struct dog.
- */
-
-/**
  * struct dog - to keep dog datas
  * @name: the dog's name
  * @age: the dog's age
