985cdefef2c28a816fd4dac9411574aa765adce3
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-16 06:55:38 -0800

    new type struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,14 @@
+/**
+ * struct dog - to keep dog datas
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ * Description: keep all dog datas
+ **/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
