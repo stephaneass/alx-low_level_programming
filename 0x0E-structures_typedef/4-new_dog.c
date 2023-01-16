92a1d97585b861cc7659ffd171151ac429e28af5
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-16 09:06:19 -0800

    creates a new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -11,15 +11,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new_dog;
-	new_dog = malloc(sizeof(dog_t));
+	dog_t *newDog;
+	newDog = malloc(sizeof(dog_t));
 
-	if (new_dog == NULL)
+	if (newDog == NULL)
 		return (NULL);
 
-	new_dog->name = name;
-	new_dog->age = age;
-	new_dog->owner = owner;
+	newDog->name = name;
+	newDog->age = age;
+	newDog->owner = owner;
 
-	return (new_dog);
+	return (newDog);
 }
