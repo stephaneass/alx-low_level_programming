8609b251b99d57e410e8181b84f4fd66723bb626
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-19 06:23:50 -0800

    function that prints numbers, followed by a new line.

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,9 +12,9 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list list;
 	unsigned int i;
 
-	if (n < 0)
+	if (n <= 0)
 		return;
-	va_start(list, n)
+	va_start(list, n);
 	for (i = 0; i < n; i++)
 	{
 		printf("%d", va_arg(list, int));
