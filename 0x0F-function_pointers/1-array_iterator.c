651cb9878171084e29b3df1b9f93b484eec61147
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-19 02:23:23 -0800

    a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-	 if (array == NULL || size == NULL || size < 1 || action == NULL)
+	 if (array == NULL || size < 1 || action == NULL)
 		 return;
 
 	 while(array)
