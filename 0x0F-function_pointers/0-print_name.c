8e2f77617b1dd0af519311fe2290bed9d16da548
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-18 18:51:36 -0800

    prints a name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * print_name - initializes a variable
+ * @name: name.
+ * @f: pointer.
+ *
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL)
+		return;
+	if (f == NULL)
+		return;
+	f(name);
+}
