fddcf3381d868e999e48a5b74f15a3b5cd3d929c
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-19 03:36:48 -0800

    a function given as a parameter on each element of an array

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,5 +1,4 @@
 #include "function_pointers.h"
-#include <stdlib.h>
 
 /**
  * array_iterator - Enter point
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,7 +1,9 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
 
-#define size_t int
+#include <stdlib.h>
+#include <stdio.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 
