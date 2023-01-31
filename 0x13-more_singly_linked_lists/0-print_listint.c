b9c307997a2292c13ae41ed5eee922b76f947b36
Stephane ASSOCLE <mensste30@gmail.com>
2023-01-31 05:22:48 -0800

    betty

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,16 +8,16 @@
  */
 size_t print_listint(const listint_t *h)
 {
-    size_t count = 0;
-    const listint_t *temp = h;
+	size_t count = 0;
+	const listint_t *temp = h;
 
-    while  (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-        /*if (temp->next != NULL)*/
-        temp = temp->next;
-        count++;
-    }
+	while  (temp != NULL)
+    	{
+        	printf("%d\n", temp->n);
+	        /*if (temp->next != NULL)*/
+        	temp = temp->next;
+	        count++;
+	}
 
-    return (count);
+	return (count);
 }
