c3ade34d6fafcfa8e7bfc56c209e33ce33673c70
Stephane ASSOCLE <mensste30@gmail.com>
2023-03-17 17:08:53 -0700

    0. Print list

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - main point
+ * dlistint_t: the list of nodes
+ *
+ * Return: the number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while(h != NULL)
+	{
+		print("%d\n", h->n);
+		h = h->next;
+
+		i++;
+	}
+	return (i);
+}
